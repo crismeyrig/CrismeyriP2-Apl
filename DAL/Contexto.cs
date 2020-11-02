eb7cfc0671e4be1b4ee9d3f2ef4ffe5edab6662f
Crismeyri Gutierrez Gil <crismeyri_gutierrez@ucne.edu.do>
2020-11-02 18:04:50 -0400

    agregando para el  examen

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,1 +1,24 @@
 
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+//using CrismeyriP2_Apl.Entidades;
+
+namespace CrismeyriP2_Apl.DAL
+{
+    public class Contexto : DbContext
+    {
+       
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= Data\Proyecto.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
